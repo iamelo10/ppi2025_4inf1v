c571c7a18af56cc4caf7e07122f5d66e70c3524a
iamelo10 <iasmimmelo900@gmail.com>
2025-06-12 19:19:22 +0000

    atividade avaliativa

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./style/theme.css";
 import "./style/global.css";
 import { MyTextList } from "./components/MyTextList";
 import { MyGrid } from "./components/MyGrid"; 
+/*import {Apple} from "lucide-react";*/
+
 export default function App() {
 
   return (
     //React Fragment
     <>
-        <MyGrid/>
+        <MyGrid />
     </>
+
+    
   );
 }
--- src/components/MyGrid.jsx
+++ src/components/MyGrid.jsx
@@ -1,39 +1,62 @@
 import style from './MyGrid.module.css';
+import { Instagram } from 'lucide-react';
+import { MessageCircle } from 'lucide-react';
+import { Github } from 'lucide-react';
 
 export function MyGrid() {
   return (
     <div className={style.container}>
-        <header className={style.header1}/>
-        <header className={style.header2}/>
-        <aside  className={style.aside}/>
-        <aside  className={style.aside2}/>
+        <header className={style.header}/>
         <main  className={style.main}/>
         <div className={style.main}>
             <div className={style.grid}>
                 <div className={style.card}>
+                    <img src="https://picsum.photos/200/200?random=1"></img>
                     <h2>Card 1</h2>
                     <p>This is the first card.</p>
                 </div>
+
                 <div className={style.card}>
+                    <img src="https://picsum.photos/200/200?random=2"></img>
                     <h2>Card 2</h2>
                     <p>This is the first card.</p>
                 </div>
+
                 <div className={style.card}>
+                    <img src="https://picsum.photos/200/200?random=3"></img>
                     <h2>Card 3</h2>
                     <p>This is the first card.</p>
                 </div>
+
                 <div className={style.card}>
+                    <img src="https://picsum.photos/200/200?random=4"></img>
                     <h2>Card 4</h2>
                     <p>This is the first card.</p>
                 </div>
+
                 <div className={style.card}>
+                    <img src="https://picsum.photos/200/200?random=5"></img>
                     <h2>Card 5</h2>
                     <p>This is the first card.</p>
                 </div>
             </div>
         </div>
         <footer className={style.footer}/>
-        <footer className={style.footer2}/>
-    </div>
+        <div className={style.footer}>
+        <p>IFRN Campus Macau</p> 
+        <p>Curso tecnico integrado em informatica</p> 
+        <p>Programação para internet 2025</p> 
+
+        <p align="center">Aluna: Iasmim Vitoria</p>
+
+        <div className={style.card}>
+            
+                <Instagram />
+                <Github />
+                <MessageCircle />
+            </div>
+        </div>
+
+        </div>
   );
 }
