fe68d06d45bb81d55e871057d03ecae4c0f1031a
iamelo10 <iasmimmelo900@gmail.com>
2025-06-13 19:08:03 +0000

    cont atividade

--- src/components/MyGrid.jsx
+++ src/components/MyGrid.jsx
@@ -1,96 +1,108 @@
-import style from './MyGrid.module.css';
-import { Instagram } from 'lucide-react';
-import { MessageCircle } from 'lucide-react';
-import { Github } from 'lucide-react';
+import style from "./MyGrid.module.css";
+import { Instagram } from "lucide-react";
+import { MessageCircle } from "lucide-react";
+import { Github } from "lucide-react";
 
 export function MyGrid() {
   return (
     <div className={style.container}>
+      <header className={style.header}></header>
+      <div className={style.header}>
+        <h1 align="center">Atividade avaliativa</h1>
+      </div>
 
-        <header className={style.header}></header>
-        <div className={style.header}>
-            
-            <h1 align="center">Atividade avaliativa</h1>
+      <main className={style.main}></main>
+      <div className={style.main}>
+        <div className={style.grid}>
+          <div className={style.card}>
+            <img src="https://picsum.photos/200/200?random=1"></img>
+
+            <h2>Card 1</h2>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              <br></br>Optio fugiat qui nam debitis doloremque esse at
+              repellendus,
+              <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum!
+              Eos quaerat pariatur voluptatem?.
+            </p>
+          </div>
+
+          <div className={style.card}>
+            <img src="https://picsum.photos/200/200?random=2"></img>
+            <h2>Card 2</h2>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              <br></br>Optio fugiat qui nam debitis doloremque esse at
+              repellendus,
+              <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum!
+              Eos quaerat pariatur voluptatem?.
+            </p>
+          </div>
+
+          <div className={style.card}>
+            <img src="https://picsum.photos/200/200?random=3"></img>
+            <h2>Card 3</h2>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              <br></br>Optio fugiat qui nam debitis doloremque esse at
+              repellendus,
+              <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum!
+              Eos quaerat pariatur voluptatem?.
+            </p>
+          </div>
+
+          <div className={style.card}>
+            <img src="https://picsum.photos/200/200?random=4"></img>
+            <h2>Card 4</h2>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              <br></br>Optio fugiat qui nam debitis doloremque esse at
+              repellendus,
+              <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum!
+              Eos quaerat pariatur voluptatem?.
+            </p>
+          </div>
+
+          <div className={style.card}>
+            <img src="https://picsum.photos/200/200?random=5"></img>
+            <h2>Card 5</h2>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              <br></br>Optio fugiat qui nam debitis doloremque esse at
+              repellendus,
+              <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum!
+              Eos quaerat pariatur voluptatem?.
+            </p>
+          </div>
         </div>
+      </div>
 
-        <main className={style.main}></main>
-        <div className={style.main}>
-            <div className={style.grid}>
-                <div className={style.card}>
-                
-                    <img src="https://picsum.photos/200/200?random=1"></img>
-                    
-                    <h2>Card 1</h2>
+      <footer className={style.footer} 
 
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                    <br></br>Optio fugiat qui nam debitis doloremque esse at repellendus,
-                    <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum! Eos quaerat pariatur voluptatem?.</p>
-
-                </div>
-
-                <div className={style.card}>
-
-                    <img src="https://picsum.photos/200/200?random=2"></img>
-                    <h2>Card 2</h2>
-
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                    <br></br>Optio fugiat qui nam debitis doloremque esse at repellendus,
-                    <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum! Eos quaerat pariatur voluptatem?.</p>
-                </div>
-
-                <div className={style.card}>
-
-                    <img src="https://picsum.photos/200/200?random=3"></img>
-                    <h2>Card 3</h2>
-
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                    <br></br>Optio fugiat qui nam debitis doloremque esse at repellendus,
-                    <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum! Eos quaerat pariatur voluptatem?.</p>
-                </div>
-
-                <div className={style.card}>
-
-                    <img src="https://picsum.photos/200/200?random=4"></img>
-                    <h2>Card 4</h2>
-
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                    <br></br>Optio fugiat qui nam debitis doloremque esse at repellendus,
-                    <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum! Eos quaerat pariatur voluptatem?.</p>
-                </div>
-
-                <div className={style.card}>
-
-                    <img src="https://picsum.photos/200/200?random=5"></img>
-                    <h2>Card 5</h2>
-
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                    <br></br>Optio fugiat qui nam debitis doloremque esse at repellendus,
-                    <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum! Eos quaerat pariatur voluptatem?.</p>
-                </div>
-            </div>
+      <div className={style.text1}> 
+        <p align="left"> IFRN Campus Macau</p>
+        <p align="left"> Curso tecnico integrado em informatica</p>
+        <p align="left"> Programação para internet 2025</p>
+    
         </div>
-
-        <footer className={style.footer}/>
-        <div  className={style.footer}>
-       
-
-        <p> IFRN Campus Macau</p> 
-        <p> Curso tecnico integrado em informatica</p> 
-        <p> Programação para internet 2025</p> 
+        <div className={style.text2}>
 
         <p align="center">Aluna: Iasmim Vitoria</p>
 
-        
-
-         <div className={style.card}>   
-        <Instagram/>
-        <Github />
-        <MessageCircle />
-                
-            </div>
-        
+        </div>
+       
+        <div className={style.icons}>
+          <Instagram />
+          <Github />
+          <MessageCircle />
         </div>
 
-        </div>
+    </footer>
+
   );
 }
