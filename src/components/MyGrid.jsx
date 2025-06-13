436a25fa68a26793e190dec3e7bec34d11123e6a
iamelo10 <iasmimmelo900@gmail.com>
2025-06-13 13:38:46 +0000

    atividade avaliativa cont.

--- src/components/MyGrid.jsx
+++ src/components/MyGrid.jsx
@@ -6,55 +6,89 @@ import { Github } from 'lucide-react';
 export function MyGrid() {
   return (
     <div className={style.container}>
-        <header className={style.header}/>
-        <main  className={style.main}/>
+
+        <header className={style.header}></header>
+        <div className={style.header}>
+            
+            <h1 align="center">Atividade avaliativa</h1>
+        </div>
+
+        <main className={style.main}></main>
         <div className={style.main}>
             <div className={style.grid}>
                 <div className={style.card}>
+                
                     <img src="https://picsum.photos/200/200?random=1"></img>
+                    
                     <h2>Card 1</h2>
-                    <p>This is the first card.</p>
+
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    <br></br>Optio fugiat qui nam debitis doloremque esse at repellendus,
+                    <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum! Eos quaerat pariatur voluptatem?.</p>
+
                 </div>
 
                 <div className={style.card}>
+
                     <img src="https://picsum.photos/200/200?random=2"></img>
                     <h2>Card 2</h2>
-                    <p>This is the first card.</p>
+
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    <br></br>Optio fugiat qui nam debitis doloremque esse at repellendus,
+                    <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum! Eos quaerat pariatur voluptatem?.</p>
                 </div>
 
                 <div className={style.card}>
+
                     <img src="https://picsum.photos/200/200?random=3"></img>
                     <h2>Card 3</h2>
-                    <p>This is the first card.</p>
+
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    <br></br>Optio fugiat qui nam debitis doloremque esse at repellendus,
+                    <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum! Eos quaerat pariatur voluptatem?.</p>
                 </div>
 
                 <div className={style.card}>
+
                     <img src="https://picsum.photos/200/200?random=4"></img>
                     <h2>Card 4</h2>
-                    <p>This is the first card.</p>
+
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    <br></br>Optio fugiat qui nam debitis doloremque esse at repellendus,
+                    <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum! Eos quaerat pariatur voluptatem?.</p>
                 </div>
 
                 <div className={style.card}>
+
                     <img src="https://picsum.photos/200/200?random=5"></img>
                     <h2>Card 5</h2>
-                    <p>This is the first card.</p>
+
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    <br></br>Optio fugiat qui nam debitis doloremque esse at repellendus,
+                    <br></br> obcaecati quisquam harum quaerat quas rem itaque ut cum! Eos quaerat pariatur voluptatem?.</p>
                 </div>
             </div>
         </div>
+
         <footer className={style.footer}/>
-        <div className={style.footer}>
-        <p>IFRN Campus Macau</p> 
-        <p>Curso tecnico integrado em informatica</p> 
-        <p>Programação para internet 2025</p> 
+        <div  className={style.footer}>
+       
+
+        <p> IFRN Campus Macau</p> 
+        <p> Curso tecnico integrado em informatica</p> 
+        <p> Programação para internet 2025</p> 
 
         <p align="center">Aluna: Iasmim Vitoria</p>
 
-        <div className={style.card}>
-            
-                <Instagram />
-                <Github />
-                <MessageCircle />
+        
+
+         <div className={style.card}>   
+        <Instagram/>
+        <Github />
+        <MessageCircle />
+                
             </div>
+        
         </div>
 
         </div>
