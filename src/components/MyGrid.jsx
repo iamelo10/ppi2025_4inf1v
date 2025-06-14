145252ced5f3b7d7f8a7da4e2ca91c23534403bd
iamelo10 <iasmimmelo900@gmail.com>
2025-06-14 02:20:46 +0000

    ATIVIDADE AVALIATIVA 1

--- src/components/MyGrid.jsx
+++ src/components/MyGrid.jsx
@@ -7,7 +7,9 @@ export function MyGrid() {
   return (
     <div className={style.container}>
       <header className={style.header}></header>
+
       <div className={style.header}>
+        <img src="https://picsum.photos/seed/picsum/100/100"></img>
         <h1 align="center">Atividade avaliativa</h1>
       </div>
 
@@ -15,7 +17,7 @@ export function MyGrid() {
       <div className={style.main}>
         <div className={style.grid}>
           <div className={style.card}>
-            <img src="https://picsum.photos/200/200?random=1"></img>
+            <img className={style.img} src="https://picsum.photos/200/200?random=1"></img>
 
             <h2>Card 1</h2>
 
@@ -29,7 +31,7 @@ export function MyGrid() {
           </div>
 
           <div className={style.card}>
-            <img src="https://picsum.photos/200/200?random=2"></img>
+            <img className={style.img} src="https://picsum.photos/200/200?random=2"></img>
             <h2>Card 2</h2>
 
             <p>
@@ -42,7 +44,7 @@ export function MyGrid() {
           </div>
 
           <div className={style.card}>
-            <img src="https://picsum.photos/200/200?random=3"></img>
+            <img className={style.img} src="https://picsum.photos/200/200?random=3"></img>
             <h2>Card 3</h2>
 
             <p>
@@ -55,7 +57,7 @@ export function MyGrid() {
           </div>
 
           <div className={style.card}>
-            <img src="https://picsum.photos/200/200?random=4"></img>
+            <img className={style.img} src="https://picsum.photos/200/200?random=4"></img>
             <h2>Card 4</h2>
 
             <p>
@@ -68,7 +70,7 @@ export function MyGrid() {
           </div>
 
           <div className={style.card}>
-            <img src="https://picsum.photos/200/200?random=5"></img>
+            <img className={style.img} src="https://picsum.photos/200/200?random=5"></img>
             <h2>Card 5</h2>
 
             <p>
@@ -82,27 +84,24 @@ export function MyGrid() {
         </div>
       </div>
 
-      <footer className={style.footer} 
-
-      <div className={style.text1}> 
-        <p align="left"> IFRN Campus Macau</p>
-        <p align="left"> Curso tecnico integrado em informatica</p>
-        <p align="left"> Programação para internet 2025</p>
-    
+      <footer className={style.footer}>
+        <div className={style.text1}>
+          <p align="left"> IFRN Campus Macau</p>
+          <p align="left"> Curso tecnico integrado em informatica</p>
+          <p align="left"> Programação para internet 2025</p>
         </div>
         <div className={style.text2}>
-
-        <p align="center">Aluna: Iasmim Vitoria</p>
-
-        </div>
-       
-        <div className={style.icons}>
-          <Instagram />
-          <Github />
-          <MessageCircle />
+          <p align="center">Aluna: Iasmim Vitoria</p>
         </div>
 
-    </footer>
-
+        
+          <div className={style.icons}>
+            <Instagram />
+            <Github />
+            <MessageCircle />
+          </div>
+        
+      </footer>
+    </div>
   );
 }
