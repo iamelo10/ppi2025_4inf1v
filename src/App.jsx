64aff2398fd505e26c8cb5691640a78f6c5e8781
iamelo10 <iasmimmelo900@gmail.com>
2025-07-09 00:47:27 +0000

    DESAFIO PROGRAMAÇÃO 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import "./style/theme.css";
 import "./style/global.css";
-import { Header } from "./components/Header";
-import { LuckyNumber } from "./components/LuckyNumber";
 import { ProductList } from "./components/ProductList";
 
 export default function App() {
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,66 +1,66 @@
-import { CircularProgress } from "@mui/material";
+import { useEffect, useState } from "react";
 import style from "./ProductList.module.css";
-import useState from "react";
-import React, { useEffect } from "react";
+import { CircularProgress } from "@mui/material";
+import { ShoppingCart } from "lucide-react";
+
 
 export function ProductList() {
-var category = "smartphones";
-var limit = 10;
-var apiUrl = `https://dummyjson.com/products/category/${category}?limit=${limit}&select=id,thumbnail,title,price,description`;
+  var category = "smartphones";
+  var limit = 10;
+  var apiUrl = `https://dummyjson.com/products/category/${category}?limit=${limit}&select=id,thumbnail,title,price,description`;
 
-const [products, setProducts] = useState([]);
-const [loading, setLoading] = useState(true); 
-const [error, setError] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className={style.container}>
+      <h1>TJA Megastore</h1>
+
+      <div className={style.cart}>
+      <button className={style.button}> <ShoppingCart /> Adicionar ao carrinho</button>
+      </div>
 
 
-useEffect(() => {
-    async function fetchProducts(){
-        try{
-            const response = await fetch(apiUrl);
-            const data = await response.json();
-            setProducts(data.products);
-        } catch (error) {
-            setError(error);
-        } finally {
-            setLoading(false);
-        }
-    
-
- fetchProducts();
- }
-
-}, []);
-
-    return ( 
-    <div className={style.container}> 
-
-    <h1 >TJA MEGASTORY</h1>
-        {products.map((product) => (
-            <div key={product.id} className={style.productCard}>
-                <img 
-                src={product.thumbnail} alt={product.title} className={style.thumbnail}
-                />
-
-                <h2 className={style.ProductTitle}>{`${product.title}`}</h2>
-                <p className={style.ProductPrice}>Price: ${product.price}</p>
-                <p className={style.ProductDescription}>{`${product.description}`}</p>
-                
-            </div>
-
-        ))}
-        {loading && (
-            <div>
-                <CircularProgress
-                    thickness={5}
-                    style={{ margin: "2rem auto", display: "block" }}
-                    sx={{color: "var(--primary-color)"}}
-                />
-        
-            <p>Loading products...</p>
-            </div>
-                
-        )}
-
+      {products.map((product) => (
+        <div key={product.id} className={style.productCard}>
+          <img
+            src={product.thumbnail}
+            alt={product.title}
+            className={style.productImage}
+          />
+          <h2 className={style.productTitle}>{product.title}</h2>
+          <p className={style.productPrice}>Price: ${product.price}</p>
+          <p className={style.productDescription}>{product.description}</p>
+        </div>
+      ))}
+      {loading && (
+        <div>
+          <CircularProgress
+            thickness={5}
+            style={{ margin: "2rem auto", display: "block" }}
+            sx={{ color: "#001111" }}
+          />
+          <p>Loading products...</p>
+        </div>
+      )}
+      {error && <p>Error loading products: {error.message} ❌</p>}
     </div>
-    );
+  );
 }
