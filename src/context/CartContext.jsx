3aae648fb013f29b0302e8a8201cce17ac8b7e06
iamelo10 <iasmimmelo900@gmail.com>
2025-09-17 20:32:14 +0000

    17/09

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, createContext } from "react";
+import { supabase } from "../utils/supabase";   
 
 export const CartContext = createContext({
   // Context to manage the products state
